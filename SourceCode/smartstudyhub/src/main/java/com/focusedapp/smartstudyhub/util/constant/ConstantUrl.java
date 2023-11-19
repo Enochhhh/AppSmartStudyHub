e2d8ed7a9463421e1d0fd493aceb6ed04ceb686f
Phan Hồng Sơn <enochphann@gmail.com>
2023-11-19 23:50:14 +0700

    Update directory of project

--- SourceCode/smartstudyhub/src/main/java/com/focusedapp/smartstudyhub/util/constant/ConstantUrl.java
+++
@@ -1,8 +0,0 @@
-package com.focusedapp.smartstudyhub.util.constant;
-
-public class ConstantUrl {
-	
-	public static final String CLIENT_URL = "http://localhost:19006";
-	public static final String SERVER_URL = "http://localhost:8080";
-	
-}
