dc7889b73f18dd46382ddd155ce547316183169f
Phan Hồng Sơn <enochphann@gmail.com>
2024-03-22 15:28:03 +0700

    Add cors file main

--- src/main/java/com/focusedapp/smartstudyhub/SmartstudyhubApplication.java
+++ src/main/java/com/focusedapp/smartstudyhub/SmartstudyhubApplication.java
@@ -2,8 +2,11 @@ package com.focusedapp.smartstudyhub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @SpringBootApplication
+@CrossOrigin(origins ="*", methods = {RequestMethod.POST, RequestMethod.GET, RequestMethod.DELETE, RequestMethod.PUT})
 public class SmartstudyhubApplication {
 
 	public static void main(String[] args) {
