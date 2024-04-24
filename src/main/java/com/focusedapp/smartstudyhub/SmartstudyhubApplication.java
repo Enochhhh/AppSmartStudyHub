6ed397d71f1e0a6658a0147a1dd781600fc39ac2
Phan Hồng Sơn <enochphann@gmail.com>
2024-04-24 15:55:19 +0700

    Add cors

--- src/main/java/com/focusedapp/smartstudyhub/SmartstudyhubApplication.java
+++ src/main/java/com/focusedapp/smartstudyhub/SmartstudyhubApplication.java
@@ -2,8 +2,11 @@ package com.focusedapp.smartstudyhub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @CrossOrigin(origins ="*", methods = {RequestMethod.POST, RequestMethod.GET, RequestMethod.DELETE, RequestMethod.PUT})
@@ -11,5 +14,16 @@ public class SmartstudyhubApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartstudyhubApplication.class, args);
+	}
+	
+	@Bean
+    WebMvcConfigurer corsConfigurer()
+	{
+		return new WebMvcConfigurer() {
+			@SuppressWarnings("unused")
+			public void addCorsMapping(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
 	}
 }
