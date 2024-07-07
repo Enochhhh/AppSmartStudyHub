de9e107c191b2b48a001baaed944a87ff45c9054
Phan Hồng Sơn <enochphann@gmail.com>
2024-07-07 13:13:21 +0000

    Update ChatGPT

--- src/main/java/com/focusedapp/smartstudyhub/config/chatgpt/OpenAIConfig.java
+++ src/main/java/com/focusedapp/smartstudyhub/config/chatgpt/OpenAIConfig.java
@@ -1,6 +1,5 @@
 package com.focusedapp.smartstudyhub.config.chatgpt;
 
-import java.util.Base64;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -12,15 +11,13 @@ import org.springframework.web.client.RestTemplate;
 public class OpenAIConfig {
 
 	@Value("${openai.api.key}")
-	String openApiKeyEncoded;
+	String openApiKey;
 	
 	@Bean
 	public RestTemplate restTemplate() {
-		byte[] decodedBytes = Base64.getDecoder().decode(openApiKeyEncoded);
-		String openApiKeyDecoded = new String(decodedBytes);
 		RestTemplate restTemplate = new RestTemplate();
 		restTemplate.getInterceptors().add((request, body, execution) -> {
-			request.getHeaders().add("Authorization", "Bearer " + openApiKeyDecoded);
+			request.getHeaders().add("Authorization", "Bearer " + openApiKey);
 			return execution.execute(request, body);
 		});
 		return restTemplate;
