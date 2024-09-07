339cbe838bbdfd9090825644d052123b969306ed
Phan Hồng Sơn <enochphann@gmail.com>
2024-09-07 11:02:17 +0700

    Update Client URL for new host

--- src/main/java/com/focusedapp/smartstudyhub/util/constant/ConstantUrl.java
+++ src/main/java/com/focusedapp/smartstudyhub/util/constant/ConstantUrl.java
@@ -2,7 +2,8 @@ package com.focusedapp.smartstudyhub.util.constant;
 
 public class ConstantUrl {
 	
-	public static final String CLIENT_URL = "exp://smartstudyhub-manhtoan1312-8081.exp.direct";
+	// public static final String CLIENT_URL = "exp://smartstudyhub-manhtoan1312-8081.exp.direct";
+	public static final String CLIENT_URL = "exp://0bdzzb0-enochphan-8081.exp.direct";
 	public static final String SERVER_URL = "https://api-smart-study-hub.onrender.com";
 	public static final String URL_CLOUDINARY = "https://res.cloudinary.com/dnj5purhu";
 	public static final String URL_FOLDER = "/SmartStudyHub";	
